9a7bbff25dd26bf27877909b4589258419782c79
yutaszk <yutaszk23@gmail.com>
2016-10-17 00:43:08 +0900

    Change zenkaku/hankaku key to Home/End

--- keyboards/ergodox/keymaps/yutaszk/keymap.c
+++ keyboards/ergodox/keymaps/yutaszk/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCTL   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  Enter |
  * |--------+------+------+------+------+------|   ]  |           |   [  |------+------+------+------+------+--------|
- * | F15/~L2|   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | F16/GUI|
+ * | CtE/~L2|   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | CtH/GUI|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | LALT |  GUI |   *  | MODE |   )  |                                       |   (  | Left | Down |  UP  | Right|
  *   `----------------------------------'                                       `----------------------------------'
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,         KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,
         KC_TAB,         KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_LALT,
         KC_LCTL,        KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
-        LT(SYMB,KC_F15),KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_RBRC,
+        LT(SYMB, LCTL(KC_HOME)),KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_RBRC,
         KC_LALT,        KC_LGUI,    LSFT(KC_8), TG(RVRS),   LSFT(KC_0),
                                                             KC_RBRC,    KC_HOME,
                                                                         KC_END,
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_7,           KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSLS,
         KC_QUOT,        KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC,
                         KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_ENT,
-        KC_LBRC,        KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    GUI_T(KC_F16),
+        KC_LBRC,        KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    GUI_T(LCTL(KC_END)),
                                     RSFT(KC_9), KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,
         KC_PGUP,        KC_LBRC,
         KC_PGDN,
