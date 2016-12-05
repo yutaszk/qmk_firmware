169f15febf6c4d7dec94b41d8131a96ce6c5b527
yutaszk <yutaszk23@gmail.com>
2016-12-05 21:53:02 +0900

    Change IME keys to MHEN/HENK

--- keyboards/hhkb/keymaps/yutaszk/keymap.c
+++ keyboards/hhkb/keymaps/yutaszk/keymap.c
@@ -5,7 +5,7 @@
 
 #define BASE 0
 #define SYMB 1
-#define HHKB 2 
+#define HHKB 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,    KC_BSLS,  KC_GRV, \
   KC_TAB,   KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_RBRC,   KC_BSPC,          \
   KC_LCTL,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,                      \
-  LT(SYMB, KC_F15),  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  GUI_T(KC_F16),  MO(HHKB),                    \
+  LT(SYMB, KC_MHEN),  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  GUI_T(KC_HENK),  MO(HHKB),                    \
                     KC_LALT,  KC_LGUI,  /*        */ SFT_T(KC_SPC),    KC_RGUI,  TG(SYMB)),
 
 
