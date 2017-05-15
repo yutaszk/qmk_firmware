768c95cafb72eaf6bfe030af1f2c0af87ea61927
yutaszk <yutaszk23@gmail.com>
2017-05-16 00:45:34 +0900

    Modify paren keys to also behave symb layer key

--- keyboards/ergodox/keymaps/yutaszk/keymap.c
+++ keyboards/ergodox/keymaps/yutaszk/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|   ]  |           |   [  |------+------+------+------+------+--------|
  * | CtE/~L2|   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | CtH/GUI|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LALT |  GUI |   *  | MODE |   )  |                                       |   (  | Left | Down |  UP  | Right|
+ *   | LALT |  GUI |   *  | MODE | )/~L2|                                       | (/~L2| Left | Down |  UP  | Right|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |   ]  | HOME |       | PgUp |   [  |
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ALT_T(KC_TAB),  KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_LALT,
         KC_LCTL,        KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
         LT(SYMB, KC_MHEN),KC_Z,     KC_X,       KC_C,       KC_V,       KC_B,       KC_RBRC,
-        KC_LALT,        KC_LGUI,    KC_ASTR,    TG(RVRS),   KC_RPRN,
+        KC_LALT,        KC_LGUI,    KC_ASTR,    TG(RVRS),   LT(SYMB, KC_RPRN),
                                                             KC_RBRC,    KC_HOME,
                                                                         KC_END,
                                              SFT_T(KC_SPC), KC_LGUI,    TG(RVRS),
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_QUOT,        KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC,
                         KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_ENT,
         KC_LBRC,        KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    GUI_T(KC_HENK),
-                                    KC_LPRN,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,
+                       LT(SYMB, KC_LPRN),    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,
         KC_PGUP,        KC_LBRC,
         KC_PGDN,
         TG(SYMB),       LT(SYMB,KC_ENT), SFT_T(KC_SPC)
