d5158da1819f34aca3f44a22c8bbee15c4bbca4a
yutaszk <yutaszk23@gmail.com>
2017-05-21 23:18:09 +0900

    Modify right modifiers

--- keyboards/ergodox/keymaps/yutaszk/keymap.c
+++ keyboards/ergodox/keymaps/yutaszk/keymap.c
@@ -14,9 +14,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  ALT |           |  DEL |   Y  |   U  |   I  |   O  |   P  |   BS   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCTL   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  Enter |
+ * | LCTL   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | Ent/GUI|
  * |--------+------+------+------+------+------|   `  |           |   '  |------+------+------+------+------+--------|
- * |MHEN/~L2|   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |HENK/GUI|
+ * |MHEN/~L2|   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |HENK/~L2|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | LALT |  GUI |   *  | MODE | )/~L2|                                       | (/~L2| Left | Down |  UP  | Right|
  *   `----------------------------------'                                       `----------------------------------'
@@ -43,9 +43,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
         KC_7,           KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSLS,
         KC_DEL,         KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC,
-                        KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_ENT,
-        KC_QUOT,        KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    GUI_T(KC_HENK),
                        LT(SYMB, KC_LPRN),    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,
+                        KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    GUI_T(KC_ENT),
+        KC_QUOT,        KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    LT(SYMN, KC_HENK),
         KC_PGUP,        KC_LBRC,
         KC_PGDN,
         KC_NO,         LT(SYMB,KC_ENT), SFT_T(KC_SPC)
@@ -57,9 +57,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  LGUI  |      |      |      |      |      |------|           |------|      |      |      |      |      |        |
+ * |  LGUI  |      |      |      |      |      |------|           |------|      |      |      |      |      | Ent/CTL|
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |HENK/CTL|
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      | MODE |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -84,8 +84,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // right hand
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-               KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,CTL_T(KC_HENK),
+               KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,CTL_T(KC_ENT),
+       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,
        KC_TRNS,
