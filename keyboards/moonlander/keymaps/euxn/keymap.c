cded157ec130c8fa5dea62ed63564167219dcf41
euxn23 <euxn23@gmail.com>
2021-07-19 19:07:59 +0900

    fix(moonlander): fix paren and brace keymaps

--- keyboards/moonlander/keymaps/euxn/keymap.c
+++ keyboards/moonlander/keymaps/euxn/keymap.c
@@ -15,16 +15,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 LALT_T(KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    MO(SYMB),         MO(SYMB), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_GRV,            KC_QUOT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
 LSFT_T(KC_MHEN), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_HENK),
-        KC_LALT, _______, _______, _______, KC_RBRC,          MO(SYMB),         MO(SYMB),          KC_LBRC, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
-                                     LSFT_T(KC_SPC), KC_LGUI, MO(SYMB),         MO(SYMB), MO(SYMB),LSFT_T(KC_SPC)
+        KC_LALT, _______, _______, _______, KC_RPRN,          MO(SYMB),         MO(SYMB),          KC_LPRN, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
+                                     LSFT_T(KC_SPC), KC_LGUI, KC_LBRC,           KC_LBRC, KC_SPC,  LSFT_T(KC_SPC)
     ),
 
     [SYMB] = LAYOUT_moonlander(
         RESET,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,             KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  RGB_TOG,
         _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, KC_MPLY,
+        _______, _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, KC_RPRN,          _______,           _______,          KC_LPRN, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,
+        _______, _______, _______, _______, _______,          _______,           _______,          KC_MPLY, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,
                                             _______, _______, _______,           _______, _______, _______
     ),
 };
